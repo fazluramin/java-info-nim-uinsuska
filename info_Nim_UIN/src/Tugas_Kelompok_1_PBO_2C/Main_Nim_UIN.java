a8bbf9bffd940016c2e8c49a5817b04e5e60d5ce
Fazlur Rahman <spasamin@gmail.com>
2019-08-15 11:23:26 +0700

    added files + revision

---
+++ info_Nim_UIN/src/Tugas_Kelompok_1_PBO_2C/Main_Nim_UIN.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_Kelompok_1_PBO_2C;
+
+/**
+ *
+ * @author ZUL
+ */
+public class Main_Nim_UIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
